c9e8ae95b2d4dec93266d685a45ee2bc09dce4a8
dnhatanh754-web <dnhatanh754@gmail.com>
2026-09-09 17:58:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("✅ Bot đã sẵn sàng!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
 bot.run(os.getenv("TOKEN"))
